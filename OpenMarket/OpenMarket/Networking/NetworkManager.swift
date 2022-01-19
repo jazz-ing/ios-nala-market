9bfd81b9a4668d8ee1619d30b7493a67295e1bfd
yoonju lee <rsyjlee@gmail.com>
2022-01-19 14:29:33 +0900

    feat: 네트워킹 타입 및 네트워크 에러 enum 구현
    * 네트워킹 타입인 NetworkManager의 기본 틀 구현
      - get, post, patch 등 여러 기능의 기본 메소드가 될 performDataTask() 메소드 구현
    * 네트워크 에러 enum인 NetworError와 에러 메시지 구현

---
+++ OpenMarket/OpenMarket/Networking/NetworkManager.swift
@@ -0,0 +1,62 @@
+//
+//  NetworkManager.swift
+//  OpenMarket
+//
+//  Created by 이윤주 on 2022/01/19.
+//
+
+import Foundation
+
+enum NetworkError: LocalizedError {
+    case requestFail(Error)
+    case invalidResponse
+    case failedResponse(statusCode: Int)
+    case emptyData
+
+    var errorDescription: String? {
+        switch self {
+        case .requestFail:
+            return "HTTP 요청에 실패했습니다."
+        case .invalidResponse:
+            return "유효하지 않은 응답입니다."
+        case .failedResponse:
+            return "실패한 상태 코드를 받았습니다: \n"
+        case .emptyData:
+            return "데이터가 존재하지 않습니다."
+        }
+    }
+}
+
+final class NetworkManager {
+
+    private let session: URLSession
+    private let successStatusCode: Range<Int> = (200 ..< 300)
+
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    func performDataTask(with request: URLRequest,
+                         completion: @escaping (Result<Data, NetworkError>) -> Void) {
+        let task = session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(.requestFail(error)))
+            }
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            guard self.successStatusCode.contains(httpResponse.statusCode) else {
+                completion(.failure(.failedResponse(statusCode: httpResponse.statusCode)))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(.emptyData))
+                return
+            }
+
+            completion(.success(data))
+        }
+        task.resume()
+    }
+}
