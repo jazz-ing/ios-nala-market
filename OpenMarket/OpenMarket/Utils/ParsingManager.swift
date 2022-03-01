f1384238a58c51d2c0aa9ef3a294c3020225c50c
yoonju lee <rsyjlee@gmail.com>
2022-03-01 18:03:33 +0900

    chore: 폴더 구조 변경
    * Error, Controller 폴더 삭제
      - ParsingError파일의 코드를 ParsingManager파일 내부로 이동
      - ViewController를 MVVM 구조에 맞게 View 폴더로 이동
    * Resources, Utils 폴더 생성
      - Asset, LaunchScreen파일을 Resources 폴더로 이동
      - ParsingManager파일을 Utils 폴더로 이동

--- OpenMarket/OpenMarket/Utils/ParsingManager.swift
+++ OpenMarket/OpenMarket/Utils/ParsingManager.swift
@@ -7,8 +7,20 @@
 
 import Foundation
 
+enum ParsingError: LocalizedError {
+
+    case decodingFail
+
+    var errorDescription: String? {
+        switch self {
+        case .decodingFail:
+            return "디코딩에 실패했습니다"
+        }
+    }
+}
+
 struct ParsingManager {
-    
+
     private let decoder = JSONDecoder()
 
     func parse<Model: Decodable>(_ data: Data, to model: Model.Type) -> Result<Model, ParsingError> {
