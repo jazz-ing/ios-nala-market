d05f1c07a82b69f90a0cd8505ee8cc151bee8799
yoonju lee <rsyjlee@gmail.com>
2022-03-08 20:58:41 +0900

    feat: ThumbnailUseCase 타입 구현
    * 셀의 썸네일을 fetch해올 ThumbnailUseCase 구현
    * NSCache를 통한 캐시 기능 구현

---
+++ OpenMarket/OpenMarket/Views/MarketListView/MarketListCell/ViewModels/ThumbnailUseCase.swift
@@ -0,0 +1,59 @@
+//
+//  ThumbnailUseCase.swift
+//  OpenMarket
+//
+//  Created by 이윤주 on 2022/03/08.
+//
+
+import Foundation
+import UIKit.UIImage
+
+enum ThumbnailUseCaseError: LocalizedError {
+    
+    case invalidImageData
+}
+
+protocol ThumbnailUseCaseProtocol {
+    
+    func fetchThumbnail(
+        from urlString: String,
+        completion: @escaping (Result<UIImage, Error>) -> Void
+    ) -> Cancellable?
+}
+
+final class ThumbnailUseCase: ThumbnailUseCaseProtocol {
+    
+    private let networkManager: NetworkManageable
+    private var imageCache = NSCache<NSString, UIImage>()
+    
+    init(networkManager: NetworkManageable = NetworkManager()) {
+        self.networkManager = networkManager
+    }
+    
+    func fetchThumbnail(
+        from urlString: String,
+        completion: @escaping (Result<UIImage, Error>) -> Void
+    ) -> Cancellable? {
+        let cacheKey = NSString(string: urlString)
+        if let cachedImage = imageCache.object(forKey: cacheKey) {
+            completion(.success(cachedImage))
+            return nil
+        }
+        
+        let task = networkManager.request(to: urlString) { [weak self] result in
+            switch result {
+            case .success(let data):
+                guard let image = UIImage(data: data) else {
+                    completion(.failure(ThumbnailUseCaseError.invalidImageData))
+                    return
+                }
+                self?.imageCache.setObject(image, forKey: cacheKey)
+                completion(.success(image))
+            case .failure(let networkError):
+                completion(.failure(networkError))
+            }
+        }
+        task?.resume()
+        return task
+    }
+}
