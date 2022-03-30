01aa6a22492bc0c202f7f6d4e2d939ffbcc30332
yoonju lee <rsyjlee@gmail.com>
2022-03-30 23:14:32 +0900

    feat: 상품 등록 화면 뷰 요소 및 제약 구현

---
+++ OpenMarket/OpenMarket/Views/ProductAddView/ProductAddViewController.swift
@@ -0,0 +1,254 @@
+//
+//  ProductAddViewController.swift
+//  OpenMarket
+//
+//  Created by 이윤주 on 2022/03/28.
+//
+
+import UIKit
+
+final class ProductAddViewController: UIViewController {
+
+    private var viewModel: ProductAddViewModel?
+
+    private let contentScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = .white
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+
+    private let contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private let photoCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = Style.PhotoCollectionView.imageSectionInsets
+        layout.itemSize = Style.PhotoCollectionView.imageItemSize
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(PhotoAddCollectionViewCell.self,
+                                forCellWithReuseIdentifier: PhotoAddCollectionViewCell.identifier)
+        return collectionView
+    }()
+
+    private let nameTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.isScrollEnabled = false
+        textView.text = Style.PlaceHolderText.name
+        textView.textColor = .systemGray3
+        textView.layer.masksToBounds = true
+        textView.layer.cornerRadius = Style.TextView.cornerRadius
+        textView.layer.borderWidth = Style.TextView.borderWidth
+        textView.layer.borderColor = UIColor.systemGray3.cgColor
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+
+    private let stockTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.isScrollEnabled = false
+        textView.text = Style.PlaceHolderText.stock
+        textView.textColor = .systemGray3
+        textView.layer.masksToBounds = true
+        textView.layer.cornerRadius = Style.TextView.cornerRadius
+        textView.layer.borderWidth = Style.TextView.borderWidth
+        textView.layer.borderColor = UIColor.systemGray3.cgColor
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+
+    private let priceStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = Style.spacing / 2
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    private let currencyPickerView: UIPickerView = {
+        let pickerView = UIPickerView()
+        return pickerView
+    }()
+
+    private let currencyTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.text = Style.PlaceHolderText.currency
+        textView.textColor = .systemGray3
+        textView.setContentHuggingPriority(.required, for: .horizontal)
+        textView.setContentCompressionResistancePriority(.required, for: .horizontal)
+        textView.isScrollEnabled = false
+        textView.layer.masksToBounds = true
+        textView.layer.cornerRadius = Style.TextView.cornerRadius
+        textView.layer.borderWidth = Style.TextView.borderWidth
+        textView.layer.borderColor = UIColor.systemGray3.cgColor
+        return textView
+    }()
+
+    private let priceTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.isScrollEnabled = false
+        textView.text = Style.PlaceHolderText.price
+        textView.textColor = .systemGray3
+        textView.layer.masksToBounds = true
+        textView.layer.cornerRadius = Style.TextView.cornerRadius
+        textView.layer.borderWidth = Style.TextView.borderWidth
+        textView.layer.borderColor = UIColor.systemGray3.cgColor
+        return textView
+    }()
+
+    private let discountedTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.isScrollEnabled = false
+        textView.text = Style.PlaceHolderText.discountedPrice
+        textView.textColor = .systemGray3
+        textView.layer.masksToBounds = true
+        textView.layer.cornerRadius = Style.TextView.cornerRadius
+        textView.layer.borderWidth = Style.TextView.borderWidth
+        textView.layer.borderColor = UIColor.systemGray3.cgColor
+        return textView
+    }()
+
+    private let passwordTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.isScrollEnabled = false
+        textView.text = Style.PlaceHolderText.password
+        textView.textColor = .systemGray3
+        textView.layer.masksToBounds = true
+        textView.layer.cornerRadius = Style.TextView.cornerRadius
+        textView.layer.borderWidth = Style.TextView.borderWidth
+        textView.layer.borderColor = UIColor.systemGray3.cgColor
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+
+    private let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.text = Style.PlaceHolderText.description
+        textView.textColor = .systemGray3
+        textView.isScrollEnabled = false
+        textView.layer.masksToBounds = true
+        textView.layer.cornerRadius = Style.TextView.cornerRadius
+        textView.layer.borderWidth = Style.TextView.borderWidth
+        textView.layer.borderColor = UIColor.systemGray3.cgColor
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setConstraints()
+    }
+}
+
+// MARK: - View Configuring Method
+
+extension ProductAddViewController {
+
+    private func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(contentScrollView)
+        contentScrollView.addSubview(contentView)
+        contentView.addSubview(photoCollectionView)
+        contentView.addSubview(nameTextView)
+        contentView.addSubview(stockTextView)
+        contentView.addSubview(priceStackView)
+        contentView.addSubview(passwordTextView)
+        contentView.addSubview(descriptionTextView)
+        priceStackView.addArrangedSubview(currencyTextView)
+        priceStackView.addArrangedSubview(priceTextView)
+        priceStackView.addArrangedSubview(discountedTextView)
+    }
+
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            contentScrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentScrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            contentScrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            contentScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            contentView.topAnchor.constraint(equalTo: contentScrollView.contentLayoutGuide.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: contentScrollView.contentLayoutGuide.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: contentScrollView.contentLayoutGuide.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: contentScrollView.contentLayoutGuide.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: contentScrollView.frameLayoutGuide.widthAnchor),
+
+            photoCollectionView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            photoCollectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            photoCollectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            photoCollectionView.heightAnchor.constraint(equalTo: photoCollectionView.widthAnchor, multiplier: Style.PhotoCollectionView.heightRatio),
+
+            nameTextView.topAnchor.constraint(equalTo: photoCollectionView.bottomAnchor, constant: Style.spacing),
+            nameTextView.leadingAnchor.constraint(equalTo: contentView.readableContentGuide.leadingAnchor),
+            nameTextView.trailingAnchor.constraint(equalTo: contentView.readableContentGuide.trailingAnchor),
+
+            stockTextView.topAnchor.constraint(equalTo: nameTextView.bottomAnchor, constant: Style.spacing),
+            stockTextView.leadingAnchor.constraint(equalTo: contentView.readableContentGuide.leadingAnchor),
+            stockTextView.trailingAnchor.constraint(equalTo: contentView.readableContentGuide.trailingAnchor),
+
+            priceStackView.topAnchor.constraint(equalTo: stockTextView.bottomAnchor, constant: Style.spacing),
+            priceStackView.leadingAnchor.constraint(equalTo: contentView.readableContentGuide.leadingAnchor),
+            priceStackView.trailingAnchor.constraint(equalTo: contentView.readableContentGuide.trailingAnchor),
+
+            passwordTextView.topAnchor.constraint(equalTo: priceStackView.bottomAnchor, constant: Style.spacing),
+            passwordTextView.leadingAnchor.constraint(equalTo: contentView.readableContentGuide.leadingAnchor),
+            passwordTextView.trailingAnchor.constraint(equalTo: contentView.readableContentGuide.trailingAnchor),
+
+            descriptionTextView.topAnchor.constraint(equalTo: passwordTextView.bottomAnchor, constant: Style.spacing),
+            descriptionTextView.leadingAnchor.constraint(equalTo: contentView.readableContentGuide.leadingAnchor),
+            descriptionTextView.trailingAnchor.constraint(equalTo: contentView.readableContentGuide.trailingAnchor),
+            descriptionTextView.bottomAnchor.constraint(equalTo: contentView.readableContentGuide.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - NameSpace
+
+extension ProductAddViewController {
+
+    private enum Style {
+
+        static let spacing: CGFloat = 10
+
+        enum PhotoCollectionView {
+            static let imageSectionInsets: UIEdgeInsets = UIEdgeInsets(top: 20,
+                                                                       left: 10,
+                                                                       bottom: 10,
+                                                                       right: 10)
+            static let imageItemSize: CGSize = CGSize(width: 80, height: 80)
+            static let heightRatio: CGFloat = 0.3
+        }
+
+        enum TextView {
+            static let cornerRadius: CGFloat = 10
+            static let borderWidth: CGFloat = 1
+        }
+
+        enum PlaceHolderText {
+            static let name = "글 제목"
+            static let stock = "재고"
+            static let currency = "통화"
+            static let price = "가격"
+            static let discountedPrice = "할인 가격"
+            static let password = "비밀번호"
+            static let description = "상품 정보를 입력해주세요."
+        }
+    }
+}
+
+
